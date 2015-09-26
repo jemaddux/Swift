820c77e115b8136d34e8ff69f0dac83e4dc0a1b3
John Maddux <jemaddux@gmail.com>
2015-09-26 19:02:38 -0400

    Done

--- guessingGame/ViewController.swift
+++ guessingGame/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var numberGuessed: UITextField!
+    @IBOutlet weak var resultField: UILabel!
+    
+    @IBAction func guessButton(sender: AnyObject) {
+        let randomNumber = Int(arc4random_uniform(6))
+        let integerGuessed = Int(numberGuessed.text!)
+        if( integerGuessed == randomNumber){
+            // You were right
+            resultField.text = "You got it!"
+        }else if( integerGuessed > randomNumber){
+            // You were too high the number was xx
+            resultField.text = "You were too high. The number was \(randomNumber)"
+        }else{
+            // You were too low, the number was yy
+            resultField.text = "You were too low. The number was \(randomNumber)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
