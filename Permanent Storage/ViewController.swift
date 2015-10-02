fa622081f11b32767791f81062ace9057530ef03
John Maddux <jemaddux@gmail.com>
2015-10-01 20:01:40 -0400

    ego splash screen

--- Permanent Storage/ViewController.swift
+++ Permanent Storage/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSUserDefaults.standardUserDefaults().setObject("Rob", forKey: "name")
+        var userName = NSUserDefaults.standardUserDefaults().objectForKey("name")
+        print(userName)
     }
 
     override func didReceiveMemoryWarning() {
