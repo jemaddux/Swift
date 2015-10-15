f7c328bd837728e7cd453d1fac55097da8af11e2
John Maddux <jemaddux@gmail.com>
2015-10-15 18:53:49 -0400

    http

--- What is the Weather/What is the Weather/ViewController.swift
+++ What is the Weather/What is the Weather/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBAction func getWeatherButton(sender: AnyObject) {
+        let url = NSURL(string: "http://www.weather-forecast.com/locations/Paris/forecasts/latest")!
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error ) -> Void in
+            if let urlContent = data {
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                print(webContent)
+            }
+        }
+        task.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
