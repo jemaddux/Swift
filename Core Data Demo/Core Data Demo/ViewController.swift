20e3e1f0b76906d9145dd78180da9a3793b147aa
John Maddux <jemaddux@gmail.com>
2015-12-03 21:33:22 -0500

    done

--- Core Data Demo/Core Data Demo/ViewController.swift
+++ Core Data Demo/Core Data Demo/ViewController.swift
@@ -7,13 +7,45 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Creating object and saving to database
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDel.managedObjectContext
         
+        
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context)
+        newUser.setValue("John", forKey: "username")
+        newUser.setValue("1234", forKey: "password")
+        do {
+            try context.save()
+            
+        } catch {
+            print("Could not save")
+        }
+
+
+        // Loading object back
+        let request = NSFetchRequest(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        do{
+            let results = try context.executeFetchRequest(request)
+            print(results)
+            if (results.count > 0){
+                for result in results as! [NSManagedObject]{
+                    print(result.valueForKey("username")!)
+                    print(result.valueForKey("password")!)
+                }
+            }
+        }catch{
+            print("Cannot print results")
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
