3bdd5cd248b18ad5738a7a4d29725e9ae30ef725
John Maddux <jemaddux@gmail.com>
2016-10-28 18:31:40 -0400

    No RunCoach in this git repo

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,3 +6,11 @@ var str = "Hello, playground"
 
 var dict = {}
 
+var count = 0
+func countUp() {
+    count += 1
+}
+
+var timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: count, selector: "countUp", userInfo: nil, repeats: true)
+timer.invalidate()
+
